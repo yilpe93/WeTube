296f8983844ab52f5944b042ced8740454c3f642
yilpe <yilpe@naver.com>
2019-03-02 15:44:08 +0900

    Initial Express Server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.get('/', function(req, res) {
+    res.send('Hello Wordl');
+});
+
+app.listen(PORT, handleListening);
