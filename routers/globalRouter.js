298faa6200c64543aa2070c4cdeb86e91b414b83
yilpe <yilpe@naver.com>
2019-03-19 15:45:23 +0900

    Update Router

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,24 +1,48 @@
 import exrpess from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
 import {
-  getJoin,
-  postJoin,
-  getLogin,
-  postLogin,
-  logout
+    getJoin,
+    postJoin,
+    getLogin,
+    postLogin,
+    githubLogin,
+    postGithubLogin,
+    getMe,
+    logout,
+    facebookLogin,
+    postFacebookLogin
 } from "../controller/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = exrpess.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate("github", { failureRedirect: "/login" }),
+    postGithubLogin
+);
+
+globalRouter.get(routes.me, getMe);
+
+globalRouter.get(routes.facebook, facebookLogin);
+globalRouter.get(
+    routes.facebookCallback,
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    postFacebookLogin
+);
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
 import {
-  userDetail,
-  editProfile,
-  changePassword
+    userDetail,
+    getEditProfile,
+    postEditProfile,
+    getChangePassword,
+    postChangePassword
 } from "../controller/userController";
+import { uploadAvatar, onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
